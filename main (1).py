5b53930a3e2cd2d2876dc62eb55628b6c8943df7
kezzaamacdalt <harry.macaulay@kingussiehigh.org.uk>
2025-11-12 13:54:33 +0000

    csasd

---
+++ main (1).py
@@ -0,0 +1,14 @@
+input1 = input(":")
+
+if input1 == 'c':
+    print(input1)
+    print("test")
+    input1 = input(":")
+
+elif input1 == 'd':
+    print(input1)
+    print("test2")
+    input1 = input(":") 
+    
+else:
+    input1 = input(":")
