7df19735d05d34e469bfa98d3c74ee790384d796
kezzaamacdalt <harry.macaulay@kingussiehigh.org.uk>
2025-11-12 13:50:33 +0000

    idwiaadsa
    addas

---
+++ main.py
@@ -0,0 +1,11 @@
+input1 = input(":")
+
+if input1 == 'c':
+    print("test")
+    input1 = input(":")
+elif input1 == 'd':
+    print("test2")
+    input1 = input(":") 
+    
+else:
+    input1 = input(":")
